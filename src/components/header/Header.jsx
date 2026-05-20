1949df157cd8a8553308713187387b4a2658dd97
rayaneely <melyouns2999@gmail.com>
2026-05-20 15:02:31 +0200

    Add PortfolioHeader

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <span className="header-logo">Madelyn Torff</span>
+            <ul className="header-nav">
+                <li><a href="/about" className="active">About</a></li>
+                <li><a href="/projects">Projects</a></li>
+                <li><a href="/contacts">Contacts</a></li>
+            </ul>
+        </div>
+    );
+};
+
+export default Header;
